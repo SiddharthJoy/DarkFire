abf45a4451ece5b6775b3b55cd320e182ad9ce50
Siddharth Barman Joy <siddharthjoy88@gmail.com>
2021-07-25 16:02:13 +0600

    Update Darkfire.ino

--- Darkfire.ino
+++ Darkfire.ino
@@ -4,13 +4,14 @@
 #include <ArduinoJson.h>
 #include <ESP8266HTTPClient.h>
 
-#define FIREBASE_HOST "project-ce84f-default-rtdb.firebaseio.com"
-#define FIREBASE_AUTH "hEZLG22ZTE0JnF0HWZa5tyMYXBfe9m4sQWzXwP5m"
-#define WIFI_SSID "zulfiqure"
-#define WIFI_PASSWORD "11223344"
+#define FIREBASE_HOST "project-*****-*******-rtdb.firebaseio.com" //     Firebase credentials
+#define FIREBASE_AUTH "hEZLG******************************XwP5m"  //  to integrate their service 
 
-String uid = "6xvKg4WfRacUyyb06Nr2pjj3owH2";
-String area = "Gendaria";
+#define WIFI_SSID "**********"                                    //      wifi credentials for
+#define WIFI_PASSWORD "***********"                               //  internet connection of nodemcu
+
+String uid = "6xvKg4WfRac**********jj3owH2";                      // This is user id.Every user have to find his id from his/her profile and add that here
+String area = "********";                                         // Set device information here.It will be displayed on the app.
 
 String mystring;
 int vr = A0,temp;
