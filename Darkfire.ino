0eed5b04160fb9cd7c91731a89608a8f0237e71d
Siddhartha Barman Joy <siddharthjoy88@gmail.com>
2022-02-22 01:32:47 +0600

    Add files via upload

--- Darkfire.ino
+++ Darkfire.ino
@@ -1,23 +1,16 @@
-
-// This file have to be burnt in nodemcu to program the device
-// It makes the device collect data from the sensor and send it to the firebase database
-// The wiring between nodemcu and temerature sensor is widely available on internet
-
-
 #include <ESP8266WiFi.h>
 #include <SoftwareSerial.h>
 #include <FirebaseArduino.h>
 #include <ArduinoJson.h>
 #include <ESP8266HTTPClient.h>
 
-#define FIREBASE_HOST "project-*****-*******-rtdb.firebaseio.com" //     Firebase credentials
-#define FIREBASE_AUTH "hEZLG******************************XwP5m"  //  to integrate their service 
+#define FIREBASE_HOST "project-ce84f-default-rtdb.firebaseio.com"
+#define FIREBASE_AUTH "hEZLG22ZTE0JnF0HWZa5tyMYXBfe9m4sQWzXwP5m"
+#define WIFI_SSID "zulfiqure"
+#define WIFI_PASSWORD "11223344"
 
-#define WIFI_SSID "**********"                                    //      wifi credentials for
-#define WIFI_PASSWORD "***********"                               //  internet connection of nodemcu
-
-String uid = "6xvKg4WfRac**********jj3owH2";                      // This is user id.Every user have to find his id from his/her profile and add that here
-String area = "********";                                         // Set device information here.It will be displayed on the app.
+String uid = "6xvKg4WfRacUyyb06Nr2pjj3owH2";
+String area = "Gendaria";
 
 String mystring;
 int vr = A0,temp;
